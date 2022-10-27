64054bee51ee187a7a53e53f79e05e4d12f5df51
Lais Golin <laisgolin@gmail.com>
2022-10-27 16:59:05 +0100

    quarta aula

---
+++ alura/organo/src/components/Time/index.js
@@ -0,0 +1,26 @@
+import Colaborador from '../Colaborador';
+import './Time.css';
+
+const Time = (props) => {
+  const bgColor = { backgroundColor: props.corSecundaria };
+  const borderColor = { borderColor: props.corPrimaria };
+
+  return (
+    props.colaboradores.length > 0 && (
+      <section className="time" style={bgColor}>
+        <h3 style={borderColor}>{props.nome}</h3>
+        <div className="colaboradores">
+          {props.colaboradores.map((colaborador) => (
+            <Colaborador
+              nome={colaborador.nome}
+              cargo={colaborador.cargo}
+              imagem={colaborador.imagem}
+            />
+          ))}
+        </div>
+      </section>
+    )
+  );
+};
+
+export default Time;
