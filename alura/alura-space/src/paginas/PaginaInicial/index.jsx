c7ca22116fb36832693b7017633e1e1b5450108c
Lais Golin <laisgolin@gmail.com>
2022-11-16 11:54:40 +0000

    Adicionando o menu e o banner da tela principal

--- alura/alura-space/src/paginas/PaginaInicial/index.jsx
+++ alura/alura-space/src/paginas/PaginaInicial/index.jsx
@@ -1,5 +1,23 @@
+import banner from './banner.png';
+
 import Cabecalho from '../../componentes/Cabecalho';
+import Menu from '../../componentes/Menu';
+
+import styles from './PaginaInicial.module.scss';
 
 export default function PaginaInicial() {
-  return <Cabecalho />;
+  return (
+    <>
+      <Cabecalho />
+      <main>
+        <section className={styles.principal}>
+          <Menu />
+          <div className={styles.principal__imagem}>
+            <h1>A galeria mais comleta do espaço</h1>
+            <img src={banner} alt="A imagem da terra vista do espaço." />
+          </div>
+        </section>
+      </main>
+    </>
+  );
 }
