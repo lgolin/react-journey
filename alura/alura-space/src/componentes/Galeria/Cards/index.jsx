184edcc7827ce44e06109b60a5154836b2f3cbae
Lais Golin <laisgolin@gmail.com>
2022-11-17 12:17:01 +0000

    refatorando componente cards

--- alura/alura-space/src/componentes/Galeria/Cards/index.jsx
+++ alura/alura-space/src/componentes/Galeria/Cards/index.jsx
@@ -1,29 +1,13 @@
 import React from 'react';
+import Card from './Card';
 
-import open from './open.png';
-import favorito from './favorito.png';
+import styles from '../../Galeria/Galeria.module.scss';
 
-export default function Cards({ itens, styles }) {
+export default function Cards({ itens, style }) {
   return (
     <ul className={styles.galeria__cards}>
       {itens.map((item) => {
-        return (
-          <li key={item.id} className={styles.galeria__card}>
-            <img
-              src={item.imagem}
-              alt={item.titulo}
-              className={styles.galeria__imagem}
-            />
-            <div>
-              <p className={styles.galeria__descricao}>{item.titulo}</p>
-
-              <span>
-                <img src={favorito} alt="ícone de coração de curtir" />
-                <img src={open} alt="ícone de abrir modal" />
-              </span>
-            </div>
-          </li>
-        );
+        return <Card key={item.id} item={item} style={style} />;
       })}
     </ul>
   );
